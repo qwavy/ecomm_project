636781bc900e73abfeff1c5f23d356615e798a68
unknown <nursultanr585@gmail.com>
2023-11-17 00:57:07 +0600

    fix header , add banner 1 ,  add some products and give styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,87 @@
-import logo from '../images/Logo.png';
 import '../components/App.css';
+
+
+
+import logo from '../images/logo.png';
+import pfp from '../images/pfp.png';
+import cart_icon from '../images/cart_icon.png';
+import search_icon from '../images/search_icon.png';
+
+import banner_1 from '../images/banner_1.png';
+
+import product_1 from '../images/product_1.png'; 
+
 const App = () => {
   return (
     <div className='site'>
       <div className="header">
+        <div>
         <img src={logo} />
+        </div>
+        
         <div className='header__navbar'>
-          {/* <ul>
-            <li><a>Laptops</a></li>
-            <li><a>Desktop PCs</a></li>
-            <li><a>Networking Devices</a></li>
-            <li><a>Printers & Scanners</a></li>
-            <li><a>PC Parts</a></li>
-            <li><a>All Other Products</a></li>
-            <li><a>Repairs</a></li>
-          </ul> */}
-          <div>
-          <a>Laptops</a>
-            <a>Desktop PCs</a>
-            <a>Networking Devices</a>
-            <a>Printers & Scanners</a>
-            <a>PC Parts</a>
-            <a>All Other Products</a>
-            <a>Repairs</a>
-          <button>Our Deals</button>
-          </div>
+            <p>Laptops</p>
+            <p>Desktop PCs</p>
+            <p>Networking Devices</p>
+            <p>Printers & Scanners</p>
+            <p>PC Parts</p>
+            <p>All Other Products</p>
+            <p>Repairs</p>
+            <button>Our Deals</button>
 
         </div>
         <div className='header__profile'>
-
+          <div>
+            <img src={search_icon}/>
+          </div>
+          <div>
+            <img src={cart_icon}/>
+          </div>
+          <div>
+            <img src={pfp}/>
+          </div>
+          
+          
+          
         </div>
       </div>
-
+      <div className='content'>
+        <div className='content__banner_1'>
+          <img src={banner_1}/>
+        </div>
+        <div className='content__products'>
+          <div className='content__product'>
+            <img src={product_1}/>
+            <h3 className='content__product__title'>EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...</h3>
+            <span className='content__product__price'>$499.00</span>
+          </div>
+          <div className='content__product'>
+            <img src={product_1}/>
+            <h3 className='content__product__title'>EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...</h3>
+            <span className='content__product__price'>$499.00</span>
+          </div>
+          <div className='content__product'>
+            <img src={product_1}/>
+            <h3 className='content__product__title'>EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...</h3>
+            <span className='content__product__price'>$499.00</span>
+          </div>
+          <div className='content__product'>
+            <img src={product_1}/>
+            <h3 className='content__product__title'>EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...</h3>
+            <span className='content__product__price'>$499.00</span>
+          </div>
+          <div className='content__product'>
+            <img src={product_1}/>
+            <h3 className='content__product__title'>EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...</h3>
+            <span className='content__product__price'>$499.00</span>
+          </div>
+          <div className='content__product'>
+            <img src={product_1}/>
+            <h3 className='content__product__title'>EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...</h3>
+            <span className='content__product__price'>$499.00</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
