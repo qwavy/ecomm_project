cb79197333bc4c5496e23110b761e835a6bc2767
unknown <nursultanr585@gmail.com>
2023-11-16 23:59:09 +0600

    add style to header__navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,37 @@
 import logo from '../images/Logo.png';
 import '../components/App.css';
 const App = () => {
-  return(
-    <div>
+  return (
+    <div className='site'>
       <div className="header">
-        <img src={logo}/>
+        <img src={logo} />
         <div className='header__navbar'>
-          <ul>
-            <li>Laptops</li>
-            <li>Desktop PCs</li>
-            <li>Networking Devices</li>
-            <li>Printers & Scanners</li>
-            <li>PC Parts</li>
-            <li>All Other Products</li>
-            <li>Repairs</li>
-          </ul>
+          {/* <ul>
+            <li><a>Laptops</a></li>
+            <li><a>Desktop PCs</a></li>
+            <li><a>Networking Devices</a></li>
+            <li><a>Printers & Scanners</a></li>
+            <li><a>PC Parts</a></li>
+            <li><a>All Other Products</a></li>
+            <li><a>Repairs</a></li>
+          </ul> */}
+          <div>
+          <a>Laptops</a>
+            <a>Desktop PCs</a>
+            <a>Networking Devices</a>
+            <a>Printers & Scanners</a>
+            <a>PC Parts</a>
+            <a>All Other Products</a>
+            <a>Repairs</a>
           <button>Our Deals</button>
+          </div>
+
         </div>
         <div className='header__profile'>
 
         </div>
       </div>
-      
+
     </div>
   )
 }
