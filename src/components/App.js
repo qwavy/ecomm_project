31b2248c899bad1da0e25fd58e74391f97989f12
unknown <nursultanr585@gmail.com>
2023-11-26 19:11:44 +0600

    add some products msi

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import MainPage from '../components/MainPage'
 import products_store from '../store/products_store';
 // import pr from '../store/products_store';
+
 const App = () => {
     return ( 
         <div>
