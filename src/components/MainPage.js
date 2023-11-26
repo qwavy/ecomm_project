4f52a5e7175da209682845205de0541f944ff830
unknown <nursultanr585@gmail.com>
2023-11-26 19:10:17 +0600

    add some products

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,76 +1,120 @@
 import React, { useState } from 'react';
 
-// components
+// components 
 import '../components/App.css';
 
 
-// mobx
+// mobx 
 import products_store from '../store/products_store';
 import { observer } from "mobx-react-lite"
 
-// icons for layout
-import logo from '../images/logo.png';
+// icons for layout 
+// import logo from '../images/logo.png'; 
 import pfp from '../images/pfp.png';
 import cart_icon from '../images/cart_icon.png';
 import search_icon from '../images/search_icon.png';
 import banner_1 from '../images/banner_1.png';
+import logo from '../images/Logo.png'
 
 
+import msi_laptop from '../images/msi_laptop.png'
 
 const MainPage = observer(() => {
 
-  const [products,setProducts] = useState(products_store.products)
+  const [products, setProducts] = useState(products_store.products)
+  const [msiLaptops, setMsiLaptops] = useState(products_store.msi_laptops)
+  const [msiDekstops,setMsiDekstops] = useState(products_store.msi_dekstops)
+  // console.log(products) 
 
-  // console.log(products)
+  const msiLaptopsFive = msiLaptops.slice(0, 5);
+  const msiDekstopsFive = msiDekstops.slice(0,5)
 
   return (
     <div className='site'>
       <div className="header">
         <div>
-        <img src={logo} />
+          <img src={logo} />
         </div>
-        
+
         <div className='header__navbar'>
-            <p>Laptops</p>
-            <p>Desktop PCs</p>
-            <p>Networking Devices</p>
-            <p>Printers & Scanners</p>
-            <p>PC Parts</p>
-            <p>All Other Products</p>
-            <p>Repairs</p>
-            <button>Our Deals</button>
+          <p>Laptops</p>
+          <p>Desktop PCs</p>
+          <p>Networking Devices</p>
+          <p>Printers & Scanners</p>
+          <p>PC Parts</p>
+          <p>All Other Products</p>
+          <p>Repairs</p>
+          <button>Our Deals</button>
 
         </div>
         <div className='header__profile'>
           <div>
-            <img src={search_icon}/>
+            <img src={search_icon} />
           </div>
           <div>
-            <img src={cart_icon}/>
+            <img src={cart_icon} />
           </div>
           <div>
-            <img src={pfp}/>
+            <img src={pfp} />
           </div>
-          
-          
-          
+
+
+
         </div>
       </div>
       <div className='content'>
         <div className='content__banner_1'>
-          <img src={banner_1}/>
+          <img src={banner_1} />
         </div>
         <div className='content__products'>
-        {products.map((product) => <div className='content__product' key={product.id}>
-            <img src={product.image}/>
+          {products.map((product) => <div className='content__product' key={product.id}>
+            <img src={product.image} />
             <h3 className='content__product__title'>{product.name}</h3>
             <span className='content__product__price'>{product.price}</span>
-        </div>)}
+          </div>)}
         </div>
-
+        <div className='msi_content'>
+          <div className='msi_banner'>
+            <p>msi laptops</p>
+            <p><a>all products</a></p>
+          </div>
+          <div className='msi_products'>
+            {msiLaptopsFive.map((laptop) => <div className='msi_product'>
+              <img src={laptop.image} />
+              <p>{laptop.name}</p>
+              <span>{laptop.price}</span>
+            </div>)}
+          </div>
+        </div>
+        <div className='msi_content'>
+          <div className='msi_banner'>
+            <p>msi laptops</p>
+            <p><a>all products</a></p>
+          </div>
+          <div className='msi_products'>
+            {msiDekstopsFive.map((laptop) => <div className='msi_product'>
+              <img src={laptop.image} />
+              <p>{laptop.name}</p>
+              <span>{laptop.price}</span>
+            </div>)}
+          </div>
+        </div>
+        <div className='msi_content'>
+          <div className='msi_banner'>
+            <p>msi laptops</p>
+            <p><a>all products</a></p>
+          </div>
+          <div className='msi_products'>
+            {msiLaptopsFive.map((laptop) => <div className='msi_product'>
+              <img src={laptop.image} />
+              <p>{laptop.name}</p>
+              <span>{laptop.price}</span>
+            </div>)}
+          </div>
+        </div>
       </div>
     </div>
   )
 })
 
-export default MainPage 
+export default MainPage
--- src/store/products_store.js
+++ src/store/products_store.js
@@ -1,25 +1,54 @@
-import { makeAutoObservable } from "mobx"
+import { makeAutoObservable } from "mobx" 
+ 
+ 
+import product_image_1 from '../images/products/product_1.png' 
+ 
+import msi_laptop_1 from '../images/products/msi_laptop_1.png' 
 
-
-import product_image_1 from '../images/product_1.png'
-
-
-class Product{
-
-    products=[
-        {id:1,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:499,image:product_image_1},
-        {id:2,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:199,image:product_image_1},
-        {id:3,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:299,image:product_image_1},
-        {id:4,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:399,image:product_image_1},
+import msi_dekstop_1 from '../images/products/msi_dekstop_1.png'
+class Product{ 
+ 
+    products=[ 
+        {id:1,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:499,image:product_image_1}, 
+        {id:2,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:199,image:product_image_1}, 
+        {id:3,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:299,image:product_image_1}, 
+        {id:4,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:399,image:product_image_1}, 
+    ] 
+    msi_laptops = [ 
+        {id:1,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_laptop_1}, 
+        {id:2,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_laptop_1}, 
+        {id:3,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_laptop_1}, 
+        {id:4,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_laptop_1}, 
+        {id:5,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_laptop_1}, 
+        {id:6,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_laptop_1}, 
+        {id:7,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_laptop_1}, 
+        {id:8,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_laptop_1}, 
+        {id:9,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_laptop_1}, 
+        {id:10,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_laptop_1}, 
+        {id:11,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_laptop_1}, 
+    ] 
+    msi_dekstops = [
+        {id:1,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_dekstop_1}, 
+        {id:2,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_dekstop_1}, 
+        {id:3,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_dekstop_1}, 
+        {id:4,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_dekstop_1}, 
+        {id:5,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_dekstop_1}, 
+        {id:6,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_dekstop_1}, 
+        {id:7,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_dekstop_1}, 
+        {id:8,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_dekstop_1}, 
+        {id:9,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_dekstop_1}, 
+        {id:10,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_dekstop_1}, 
+        {id:11,name:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...',price:449,image:msi_dekstop_1}, 
     ]
-    constructor(){
-        makeAutoObservable(this)
-    }
 
-    test(){
-        console.log('test')
-    }
-
-}
-
+    constructor(){ 
+        makeAutoObservable(this) 
+    } 
+ 
+    test(){ 
+        console.log('test') 
+    } 
+ 
+} 
+ 
 export default new Product()
